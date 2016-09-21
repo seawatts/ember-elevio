c988f1f4058b1aa498271d9f6921f9d84b1cf23e
Chris Watts <cwatts@simplymeasured.com>
2016-09-20 19:16:14 -0700

    Add enabled flag

--- vendor/elevio-shim.js
+++ vendor/elevio-shim.js
@@ -11,6 +11,10 @@ String.prototype.toSnakeCase = function() {
   var _elev = window._elev = {};
 
   function _setup(config) {
+    if (!config.enabled) {
+      return;
+    }
+
     var i, e;
     i = document.createElement("script"), i.type = 'text/javascript';
     i.async = 1, i.src = "https://static.elev.io/js/v3.js", e =
